89833a075eab0be5ead1352bed9843827112d4b9
hzhgit <971499086@qq.com>
2020-08-26 14:21:08 +0800

    [zach,york]feature:add should_return_players_win_when_given_not_repeat_card_but_flush_and_max_card_equals test

--- src/test/java/com/example/PokerTest.java
+++ src/test/java/com/example/PokerTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PokerTest {
     @Test
-    void should_return_black_win_when_given_not_repeat_card_and_not_flush() {
+    void should_return_players_win_when_given_not_repeat_card_and_not_flush() {
         // given
         Compare compare = new Compare();
         Player player1 = new Player(Arrays.asList("2H", "3D", "5S", "9C", "KD"));
@@ -20,5 +20,18 @@ public class PokerTest {
 
         // then
         assertEquals("player2 wins.", actual);
+    }
+
+    @Test
+    void should_return_players_win_when_given_not_repeat_card_but_flush_and_max_card_equals() {
+        // given
+        Compare compare = new Compare();
+        Player player1 = new Player(Arrays.asList("2H", "3H", "5H", "9H", "KH"));
+        Player player2 = new Player(Arrays.asList("2C", "3C", "5C", "9C", "KC"));
+        // when
+        String actual = compare.comparePoker(player1,player2);
+
+        // then
+        assertEquals("Tie.", actual);
     }
 }
